ab0e6cb4257377afb02bfd5c6a1d6882f1ca7137
tahminahesari <hesari@live.com>
2021-11-29 19:39:43 +0100

    zelf opgelost

--- src/VoetbalclubArray.java
+++ src/VoetbalclubArray.java
@@ -1,16 +1,14 @@
 public class VoetbalclubArray {
-    public VoetbalclubArray(String ajax_amsterdam, String afca, int i) {
-    }
 
     public static void main(String[] args) {
-        VoetbalclubArray ajax = new VoetbalclubArray("Ajax Amsterdam", "AFCA", 1902);
-        VoetbalclubArray psv = new VoetbalclubArray("PSV Eindhoven", "PSV", 1903);
-        VoetbalclubArray utrecht = new VoetbalclubArray("FC Utrecht", "030", 1970);
-        VoetbalclubArray az = new VoetbalclubArray("AZ Alkmaar", "AZ", 1980);
+        Voetbalclub ajax = new Voetbalclub("Ajax Amsterdam", "AFCA", 1902);
+        Voetbalclub psv = new Voetbalclub("PSV Eindhoven", "PSV", 1903);
+        Voetbalclub utrecht = new Voetbalclub("FC Utrecht", "030", 1970);
+        Voetbalclub az = new Voetbalclub("AZ Alkmaar", "AZ", 1980);
 
-        VoetbalclubArray[] clubs = {ajax,psv,utrecht,az};
-        //runt niet
-        System.out.println("Club op positie 0:" + clubs[0].getNaam());
+        Voetbalclub[] clubs = {ajax,psv,utrecht,az};
+
+        System.out.println("Club op positie 0:" + clubs[2].getNaam());
 
     }
 }
